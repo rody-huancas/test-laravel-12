f8939f8b168cf898fdb6e9ef76a488c88d83107b
Rody Huancas <rodyhuancas.04@gmail.com>
2025-04-20 18:49:20 -0500

    Feat: enhance Post model with attribute casting and title formatting; update migration for published_at and is_active fields

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,5 +10,21 @@ class Post extends Model
 {
     use HasFactory;
 
-    protected $table = 'posts';
+    // protected $table = 'posts';
+
+    protected function casts(): array
+    {
+        return [
+            'published_at' => 'datetime',
+            'is_active' => 'boolean',
+        ];
+    }
+
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => strtolower($value),
+            get: fn($value) => ucfirst($value),
+        );
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,51 +12,51 @@ Route::get('/posts/create', [PostController::class, "create"]);
 Route::get('/posts/{post}', [PostController::class, "show"]);
 
 Route::get('/prueba', function () {
-    /*
-    // crea un post
-    $post = new Post;
+    $post = Post::find(1);
 
-    $post->title = "Mi segundo post";
-    $post->content = "Contenido del post 2";
-    $post->category = "Programación";
-
-    $post->save();
-
-    return $post;
-    */
-
-    // Obtener el registro por su id
-    // $post = Post::find(1);
-
-    // actualizar resistro
-    /*
-    $post = Post::where("title", "Mi primer post")->first();
-    $post->category = "Programación nuevo";
-    $post->save();
-    */
-
-    // obtener todos los registros
-    // $post = Post::all();
-
-    // Obtener los posts cuyo id sea mayor a 1
-    // $post = Post::where("id", ">=", "1")->get();
-
-    // Obtener los posts ordenados de manera descendente por el id
-    // $post = Post::orderBy("id", "desc")->get();
-
-    // Obtener los posts ordenados y con campos especficos
-    $post = Post::orderBy("id", "desc")
-        ->select("id", "title", "content")
-        ->get();
-
-
-    // eliminar un registro
-    // $post = Post::find(1);
-    // $post->delete();
-    // return "Eliminado";
-
-    return $post;
+    return $post->is_active;
 });
+
+// Route::get('/prueba', function () {
+//     // crea un post
+//     // $post = new Post;
+//     // $post->title = "Mi PoST nUevo";
+//     // $post->content = "Contenido del post";
+//     // $post->category = "Programación";
+//     // $post->save();
+
+//     // Obtener el registro por su id
+//     // $post = Post::find(1);
+
+//     // actualizar resistro
+//     /*
+//     $post = Post::where("title", "Mi primer post")->first();
+//     $post->category = "Programación nuevo";
+//     $post->save();
+//     */
+
+//     // obtener todos los registros
+//     // $post = Post::all();
+
+//     // Obtener los posts cuyo id sea mayor a 1
+//     // $post = Post::where("id", ">=", "1")->get();
+
+//     // Obtener los posts ordenados de manera descendente por el id
+//     // $post = Post::orderBy("id", "desc")->get();
+
+//     // Obtener los posts ordenados y con campos especficos
+//     $post = Post::orderBy("id", "desc")
+//         ->select("id", "title", "content")
+//         ->get();
+
+
+//     // eliminar un registro
+//     // $post = Post::find(1);
+//     // $post->delete();
+//     // return "Eliminado";
+
+//     return $post;
+// });
 
 // Route::get('/posts/{post}/{category?}', function ($post, $category = null) {
 //     if ($category) {
