759d84e2e023bf6f56bacfab1987d65f089e2d43
Rody Huancas <rodyhuancas.04@gmail.com>
2025-04-20 14:39:47 -0500

    Feat: add Post model and implement sample routes for post retrieval

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 
 Route::get('/', [HomeController::class, "index"]);
 
@@ -10,6 +11,52 @@ Route::get('/posts', [PostController::class, "index"]);
 Route::get('/posts/create', [PostController::class, "create"]);
 Route::get('/posts/{post}', [PostController::class, "show"]);
 
+Route::get('/prueba', function () {
+    /*
+    // crea un post
+    $post = new Post;
+
+    $post->title = "Mi segundo post";
+    $post->content = "Contenido del post 2";
+    $post->category = "Programación";
+
+    $post->save();
+
+    return $post;
+    */
+
+    // Obtener el registro por su id
+    // $post = Post::find(1);
+
+    // actualizar resistro
+    /*
+    $post = Post::where("title", "Mi primer post")->first();
+    $post->category = "Programación nuevo";
+    $post->save();
+    */
+
+    // obtener todos los registros
+    // $post = Post::all();
+
+    // Obtener los posts cuyo id sea mayor a 1
+    // $post = Post::where("id", ">=", "1")->get();
+
+    // Obtener los posts ordenados de manera descendente por el id
+    // $post = Post::orderBy("id", "desc")->get();
+
+    // Obtener los posts ordenados y con campos especficos
+    $post = Post::orderBy("id", "desc")
+        ->select("id", "title", "content")
+        ->get();
+
+
+    // eliminar un registro
+    // $post = Post::find(1);
+    // $post->delete();
+    // return "Eliminado";
+
+    return $post;
+});
 
 // Route::get('/posts/{post}/{category?}', function ($post, $category = null) {
 //     if ($category) {
